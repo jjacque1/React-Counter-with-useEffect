dcd575d9bd97dbd8b8ca367554f0dc31cc40c572
jjacque1 <jacksonjacque.jj@gmail.com>
2025-12-15 16:35:36 -0500

    increment works, decrement works, reset works, working on count history next

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,26 +1,54 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Counter() {
-    const [count, setCount] = useState<number>(0);
-    const [step, setStep] = useState<number>(1);
-    const [history, setHistory] = useState<number[]>([])
+  const [count, setCount] = useState<number>(0);
+  const [step, setStep] = useState<number>(1);
+  const [history, setHistory] = useState<number[]>([]);
 
+  function incrementCount() {
+    setCount((prev: number): number => {
+      return prev + step;
+    });
+  }
 
-    function incrementCount() {
-        setCount((prev: number): number => {
-            return prev + step
-        })
+  function decrementCount() {
+    setCount((prev: number): number => {
+      return prev - step;
+    });
+  }
+
+  function resetCounter() {
+    setCount(0);
+    setHistory([]);
+  }
+
+  function handleStep(event: React.ChangeEvent<HTMLInputElement>) {
+    const newStepValue = Number(event.target.value);
+
+    if (newStepValue >= 1) {
+      setStep(newStepValue);
     }
+  }
 
-    function decrementCount() {
-        setCount((prev: number): number => {
-            return prev - step
-        })
-    }
-
-    function resetCounter() {
-        setCount(0);
-        setHistory([])
-    }
-
+  return (
+    <div>
+      <h3>Counter</h3>
+      <br />
+      <h1>Current Count: {count}</h1>
+      <div className="btns">
+            <button onClick={decrementCount}>Decrement</button>
+            <button onClick={incrementCount}>Increment</button>
+            <button onClick={resetCounter}>Reset</button>
+      </div>
+      <div>
+        <label htmlFor="step">Step Value:</label>
+        <input type="number" 
+        min={1}
+        value={step} 
+        onChange={handleStep}/>
+      </div>
+      <h3>Count History:</h3>
+      <p>{ history.join(", ") }</p>
+    </div>
+  );
 }
